82c235c2718ff143142b9ec0ab20883a14b2399e
Drake-Programming <adaptivecreature1864@gmail.com>
2025-01-08 16:50:03 -0500

    #4 Added middleware for authorization of username and token. Added error handling to api.go

--- api/api.go
+++ api/api.go
@@ -1,16 +1,21 @@
 package api
 
+import (
+	"encoding/json"
+	"net/http"
+)
+
 // Balance Params
 type BankBalanceParams struct {
 	Username string
 }
 
-// Coin Balance Response
+// Balance Response
 type BankBalanceResponse struct {
 	// Success Code
 	Code int
 
-	//Account Balance
+	// Account Balance
 	Balance int64
 }
 
@@ -21,3 +26,24 @@ type Error struct {
 	// Error message
 	Message string
 }
+
+func writeError(writer http.ResponseWriter, message string, code int) {
+	response := Error{
+		Code:    code,
+		Message: message,
+	}
+
+	writer.Header().Set("Content-Type", "application/json")
+	writer.WriteHeader(code)
+
+	json.NewEncoder(writer).Encode(response)
+}
+
+var (
+	RequestErrorHandler = func(writer http.ResponseWriter, err error) {
+		writeError(writer, err.Error(), http.StatusBadRequest)
+	}
+	InternalErrorHandler = func(writer http.ResponseWriter) {
+		writeError(writer, "An Unexpected Error Occurred.", http.StatusInternalServerError)
+	}
+)
